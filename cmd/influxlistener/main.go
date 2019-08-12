fe2871122b1fd8b53d55f58cc9f4d0800089f2f1
Joshua Adams <n7down@gmail.com>
2019-08-11 19:44:29 -0600

    fixed subscribe

--- cmd/influxlistener/main.go
+++ cmd/influxlistener/main.go
@@ -5,12 +5,19 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	client "github.com/influxdata/influxdb1-client/v2"
 	"github.com/sirupsen/logrus"
 )
+
+type Sensor struct {
+	client          client.Client
+	sensorDataPoint SensorDataPoint
+}
 
 type SensorDataPoint struct {
 	Location string `json:"location"`
@@ -78,78 +85,57 @@ func (s SensorDataPoint) LogSensor() error {
 	return nil
 }
 
-func connect(clientId string, uri *url.URL) mqtt.Client {
-	opts := createClientOptions(clientId, uri)
-	client := mqtt.NewClient(opts)
-	token := client.Connect()
-	for !token.WaitTimeout(3 * time.Second) {
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	logrus.Infof("Received message: %s\n", msg.Payload())
+
+	// unmashal payload
+	sensorDataPoint := &SensorDataPoint{}
+	err := json.Unmarshal([]byte(msg.Payload()), sensorDataPoint)
+	if err != nil {
+		logrus.Error(err.Error())
 	}
-	if err := token.Error(); err != nil {
-		logrus.Fatal(err.Error())
+
+	err = sensorDataPoint.LogSensor()
+	if err != nil {
+		logrus.Error(err.Error())
 	}
-	return client
 }
 
-func createClientOptions(clientId string, uri *url.URL) *mqtt.ClientOptions {
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(fmt.Sprintf("tcp://%s", uri.Host))
-	opts.SetUsername(uri.User.Username())
-	password, _ := uri.User.Password()
-	opts.SetPassword(password)
-	opts.SetClientID(clientId)
-	return opts
-}
+func init() {
 
-func listen(uri *url.URL, topic string) {
-	client := connect("sub", uri)
-	client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
-		logrus.Infof("* [%s] %s\n", msg.Topic(), string(msg.Payload()))
-
-		// unmashal payload
-		sensorDataPoint := &SensorDataPoint{}
-		err := json.Unmarshal([]byte(msg.Payload()), sensorDataPoint)
-		if err != nil {
-			logrus.Error(err.Error())
-		}
-
-		err = sensorDataPoint.LogSensor()
-		if err != nil {
-			logrus.Error(err.Error())
-		}
-	})
 }
 
 func main() {
-	uri, err := url.Parse(os.Getenv("MQTT_URL"))
-	if err != nil {
-		logrus.Fatal(err.Error())
-	}
-	topic := uri.Path[1:len(uri.Path)]
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	fmt.Println(fmt.Sprintf("URL: %v", uri))
-
-	go listen(uri, topic)
-
-	client := connect("pub", uri)
-	timer := time.NewTicker(1 * time.Second)
-
-	d := SensorDataPoint{
-		Location:                 "master bathroom",
-		Label:                    "1",
-		Version:                  "v0.1",
-		Device:                   "bc",
-		DHT22Humidity:            "60.0",
-		DHT22Temperature:         "23.4",
-		BMP280Temperature:        "25.0",
-		BMP280BarometricPressure: "26.0",
-	}
-
-	j, err := json.Marshal(d)
+	mqttUri, err := url.Parse(os.Getenv("MQTT_URL"))
 	if err != nil {
 		logrus.Fatal(err.Error())
 	}
 
-	for _ = range timer.C {
-		client.Publish(topic, 0, false, string(j))
+	topic := mqttUri.Path[1:len(mqttUri.Path)]
+	if topic == "" {
+		topic = "test"
 	}
+
+	opts := mqtt.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s", mqttUri.Host))
+	opts.SetUsername(mqttUri.User.Username())
+	password, _ := mqttUri.User.Password()
+	opts.SetPassword(password)
+	opts.SetClientID("influx-listener")
+	opts.SetDefaultPublishHandler(f)
+
+	opts.OnConnect = func(c mqtt.Client) {
+		if token := c.Subscribe(topic, 0, f); token.Wait() && token.Error() != nil {
+			logrus.Fatal(token.Error())
+		}
+	}
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		logrus.Fatal(token.Error())
+	} else {
+		logrus.Info("Connected to server\n")
+	}
+	<-c
 }
