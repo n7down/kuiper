bcbdaef214fb63f0d7762db035f3b96279d93c28
Joshua Adams <n7down@gmail.com>
2020-05-03 15:22:37 -0700

    changing to uint32

--- internal/settings/persistence/batcavesetting.go
+++ internal/settings/persistence/batcavesetting.go
@@ -4,7 +4,7 @@ import "time"
 
 type BatCaveSetting struct {
 	DeviceID       string `gorm:"primary_key"`
-	DeepSleepDelay int32
+	DeepSleepDelay uint32
 	CreatedAt      *time.Time `gorm:"index" json:"created_at"`
 	UpdatedAt      *time.Time `gorm:"index" json:"updated_at"`
 	DeletedAt      *time.Time `gorm:"index" json:"deleted_at"`
