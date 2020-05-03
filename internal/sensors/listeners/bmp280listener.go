63b448cb0c1ac02f03fff07067547469c01919b7
Joshua Adams <n7down@gmail.com>
2020-05-03 09:18:30 -0700

    bug

--- internal/sensors/listeners/bmp280listener.go
+++ internal/sensors/listeners/bmp280listener.go
@@ -42,7 +42,7 @@ func (e SensorsListenersEnv) NewBMP280Listener(listenerName string, bmp280MqttUR
 		}
 
 		if err == nil {
-			err = e.influxDB.LogBMP280(listenerName, sensor)
+			err = e.influxDB.LogBMP280(sensor)
 			logrus.Infof("Logged sensor: %v", sensor)
 			if err != nil {
 				logrus.Error(err.Error())
